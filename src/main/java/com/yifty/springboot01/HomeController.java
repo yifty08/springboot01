5eaac8a8845b865a181bcb218f422cc7a89ce731
Yifty <yifty08@icloud.com>
2018-07-09 20:30:17 -0400

    Initial commit

---
+++ src/main/java/com/yifty/springboot01/HomeController.java
@@ -0,0 +1,12 @@
+package com.yifty.springboot01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String homePage(){
+        return "index";
+    }
+}
